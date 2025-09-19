0c8de071633f79bd9f3968ea49436aabfdd5835b
Emily Middleton <emiddleton@smith.edu>
2025-09-19 13:41:36 -0400

    javadoc

--- Individual.java
+++ Individual.java
@@ -1,29 +1,41 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * A class for an Individual, which is an entity 
+ * primarily defined by its chromosome, or sequence of genes.
+ */
 public class Individual {
 
     /**
      * Chromosome stores the individual's genetic data as an ArrayList of characters
      * Each character represents a gene
      */
-    ArrayList<Character> chromosome;
-    public int size;
-    public int fitness;
+    public ArrayList<Character> chromosome;
+    private int size;
 
     /**
-     * Inital constructor to generate initial population members
+     * Inital constructor to generate initial population members before sexual reproduction is possible.
      * @param c_0 The initial chromosome size
-     * @param num_letters The number of letters available to choose from
+     * @param geneStates the number of states possible for a gene to take, or, the number of letters available for a gene to be
+     * @param rng a Random object for randomizing genes in the chromosome
      */
-    public Individual(int c_0, int num_letters, Random rng) {
+    public Individual(int c_0, int geneStates, Random rng) {
         this.chromosome = new ArrayList<Character>(c_0); // allocate memory with initial size
         this.size = c_0;
 
         for (int i = 0; i < c_0; i++){
-            this.chromosome.add(this.randomLetter(num_letters, rng)); // fill in chromosome with random letters
+            this.chromosome.add(randomLetter(geneStates, rng)); // fill in chromosome with random letters
         }
 
+    }
+
+    /**
+     * Getter method for the size of an Individual's chromosome.
+     * @return the size of the individual's chromosome.
+     */
+    public int getSize(){
+        return this.size;
     }
 
     /**
@@ -32,15 +44,15 @@ public class Individual {
      * @param parent2 The second parent chromosome
      * @param c_max The maximum chromosome size
      * @param m The chances per round of mutation in each gene
-     * @param num_letters the number of letters available to choose from
+     * @param geneStates the number of letters available to choose from for a given gene
      * @param rng the random number generator being used for the current run
      */
-    public Individual(Individual parent1, Individual parent2, int c_max, float m, int num_letters, Random rng) {
-        int prefix = rng.nextInt(parent1.size) + 1; // will be minimum of one, and up to the size of the parent 
-        int suffix = rng.nextInt(parent2.size) + 1;
+    public Individual(Individual parent1, Individual parent2, int c_max, float m, int geneStates, Random rng) {
+        int prefix = rng.nextInt(parent1.getSize()) + 1; // will be minimum of one, and up to the size of the parent 
+        int suffix = rng.nextInt(parent2.getSize()) + 1;
 
         int prefix_ind = prefix - 1; // change num char to the index
-        int suffix_ind = parent2.size - suffix; // counting from the end of parent2's chromosome
+        int suffix_ind = parent2.getSize() - suffix; // counting from the end of parent2's chromosome
 
         this.chromosome = new ArrayList<Character>(prefix+suffix);
         this.size = prefix+suffix;
@@ -51,27 +63,25 @@ public class Individual {
             this.chromosome.add(parent1.chromosome.get(i));
         }
 
-        for (int i = suffix_ind; i < parent2.size; i++) {
+        for (int i = suffix_ind; i < parent2.getSize(); i++) {
             this.chromosome.add(parent2.chromosome.get(i));
         }
-
         
-       while(this.size > c_max){
-        this.chromosome.remove(this.size-1);
-        this.size -= 1;
-       }
-
-       // mutations //
-
-       for (int i = 0; i < this.size; i++) {
-        Boolean mutation = this.doesMutate(m, rng);
-
-        if(mutation){
-            this.chromosome.remove(i);
-            this.chromosome.add(i, this.randomLetter(num_letters, rng));
+        // truncate the chromosome if it is longer than c_max
+        while(this.size > c_max){
+            this.chromosome.remove(this.size-1);
+            this.size -= 1;
         }
-       }
-
+        
+        // mutations //
+        for (int i = 0; i < this.size; i++) {
+            Boolean mutation = doesMutate(m, rng);
+            
+            if(mutation){
+                this.chromosome.remove(i);
+                this.chromosome.add(i, randomLetter(geneStates, rng));
+            }
+        }
     }
 
     /**
@@ -79,37 +89,37 @@ public class Individual {
      * @return the fitness score of an individual.
      */
     public int getFitness(){
-        this.fitness = 0;
+        int fitness = 0;
 
         // part 1: palindromic //
         for(int i = 0; i < this.size/2; i++) {
             if(this.chromosome.get(i).equals(this.chromosome.get(this.size-i-1))){
-                this.fitness += 1;
+                fitness += 1;
             } else {
-                this.fitness -= 1;
+                fitness -= 1;
             }
         }
         if(this.size/2 != this.size/2.f) { // if there are an odd number of chromosomes, increase by one for middle
-            this.fitness += 1;
+            fitness += 1;
         }
 
         // part 2: side by side //
         for(int i = 0; i < this.size-1; i++){
             if(this.chromosome.get(i).equals(this.chromosome.get(i+1))){
-                this.fitness -= 1;
+                fitness -= 1;
             }
         }
-        return this.fitness;
+        return fitness;
     }
 
     /**
      * Provided method to choose a letter at random, in the range from A to the number of states indicated
-     * @param num_letters The number of letters available to choose from (number of possible states)
+     * @param geneStates The number of letters available to choose from (number of possible states) for a gene
      * @param rng The random number generator being used for the current run
      * @return The letter as a Character
      */
-    private Character randomLetter(int num_letters, Random rng) {
-        return Character.valueOf((char)(65 + rng.nextInt(num_letters)));
+    private Character randomLetter(int geneStates, Random rng) {
+        return Character.valueOf((char)(65 + rng.nextInt(geneStates)));
     }
 
     /**
@@ -137,6 +147,10 @@ public class Individual {
 
 
 
+    /**
+     * Main method for testing and debugging.
+     * @param args 
+     */
     public static void main(String[] args) {
         // This code will set a random seed when you're testing Individual (i.e., running it without GA_Simulation)
         Random rng = new Random(System.currentTimeMillis());
